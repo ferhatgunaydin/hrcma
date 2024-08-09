15bf85e9a73333cc6b95cfc2b634372f5224d5d4
Ferhat Günaydın <127533159+ferhatgunaydin@users.noreply.github.com>
2024-08-09 19:10:14 -0400

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyChmx3J9-ApyEr8aBk8FMPs_dxhBpyf5HU",
+    authDomain: "hrcm-ffe46.firebaseapp.com",
+    projectId: "hrcm-ffe46",
+    storageBucket: "hrcm-ffe46.appspot.com",
+    messagingSenderId: "15808191361",
+    appId: "1:15808191361:web:070a59c6437e8398b6a680",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
